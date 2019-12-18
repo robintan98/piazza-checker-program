d32b9635139990a896982e2b248bfd40a6ce4ebd
robintan98 <46473314+robintan98@users.noreply.github.com>
2019-12-18 09:34:07 -0500

    Sorted output by last name

--- main.py
+++ main.py
@@ -22,8 +22,13 @@ def process_name(name):
         return 'Jeewon Lee'
     return name
 
+def byLastName(name):
+    return name.split(" ")[1]
+
 def process_folder(folder):
     answerer_list = []
+    if type(folder) == float:
+        return answerer_list
     folder_split = folder.split('; ')
     for answerer in folder_split:
         if 'misc' not in answerer:
@@ -46,11 +51,17 @@ def answered_on_time(time_of_question, time_of_response):
     response_time_day = int(''.join(response_time_day_split))
 
     if response_time_day - question_time_day < 2 \
-            or (response_time_day == 20191001 and question_time_day == 20190930): #If response is less than 2 days
+            or (response_time_day == 20191001 and question_time_day == 20190930) \
+            or (response_time_day == 20191101 and question_time_day == 20191031) \
+            or (response_time_day == 20191201 and question_time_day == 20191130): #If response is less than 2 days
         return True
     elif (response_time_day - question_time_day == 2) \
             or (response_time_day == 20191001 and question_time_day == 20190929) \
-            or (response_time_day == 20191002 and question_time_day == 20190930): #If response takes 2 days
+            or (response_time_day == 20191002 and question_time_day == 20190930) \
+            or (response_time_day == 20191101 and question_time_day == 20191030) \
+            or (response_time_day == 20191102 and question_time_day == 20191031) \
+            or (response_time_day == 20191201 and question_time_day == 20191129) \
+            or (response_time_day == 20191202 and question_time_day == 20191130): #If response takes 2 days
         question_time_hour_split = question_time_split[1].split(':')
         question_time_hour = int(''.join(question_time_hour_split))
         response_time_hour_split = response_time_split[1].split(':')
@@ -62,10 +73,17 @@ def answered_on_time(time_of_question, time_of_response):
     else: #If response takes more than 2 days
         return False
 
+def isCurrentSession(time_of_question):
+    question_time_split = time_of_question.split(' ')
+    question_time_day_split = question_time_split[0].split('-')
+    question_time_day = int(''.join(question_time_day_split))
+    return question_time_day >= 20191030
+
 ###Body#################################################################################################################
 
 ###Load Data
-all_data = pd.read_csv(path, encoding='cp1252')
+# all_data = pd.read_csv(path, encoding='cp1252')
+all_data = pd.read_csv(path, encoding='utf8')
 students_list = []
 dict_indices = {}
 
@@ -77,6 +95,7 @@ for name in all_data['Name']:
 students_list.sort()
 
 count = 0
+students_list.sort(key = byLastName)
 for student in students_list:
     dict_indices[student] = count
     count += 1
@@ -97,11 +116,14 @@ skip_row = False
 for index, row in all_data.iterrows():
     if row['Post Number'] != currentPost: #New Post Number
         # Skip this entire Post Number if any of the following conditions hold:
-        if row['Name'] == 'John' \
-                or '$skip123$' in row['Submission']:
+        if row['Name'] == 'John':
                 # or int(''.join(row['Created At'].split(' ')[1].split(':'))) >= 160000:
             skip_row = True
             continue
+        if type(row['Submission']) is not float:
+            if '$skip123$' in row['Submission']:
+                skip_row = True
+                continue
         responded = []
         currentPost = row['Post Number']
         skip_row = False
@@ -109,18 +131,20 @@ for index, row in all_data.iterrows():
         answerer_list = process_folder(row['Folders'])
         asker_index = dict_indices[asker]
 
+        # Updates time_of_question
+        time_of_question = row['Created At']
+
         #Updates questioned_array and correspondences
         for answerer in answerer_list:
             answerer_index = dict_indices[answerer]
 
             #Updates questioned_array
-            questioned_array[answerer_index] += 1
+            if isCurrentSession(time_of_question):
+                #Updates only if during current session
+                questioned_array[answerer_index] += 1
 
             #Updates correspondences
             correspondences[asker_index, answerer_index] += 1
-
-        #Updates time_of_question
-        time_of_question = row['Created At']
 
     else: #Post Number has been seen before
         if skip_row:
@@ -135,7 +159,9 @@ for index, row in all_data.iterrows():
                 if answered_timely:
                     #Update responded_array if responder answered on time
                     responder_index = dict_indices[responder]
-                    responded_array[responder_index] += 1
+                    if isCurrentSession(time_of_question):
+                        # Updates only if during current session
+                        responded_array[responder_index] += 1
 
 dict_percentages = {}
 for index in range(len(students_list)):
@@ -148,7 +174,7 @@ for index in range(len(students_list)):
 ###Export to files######################################################################################################
 with open('Percentage of Timely Responses.txt', 'w') as txt_file:
     txt_file.write('Percentages of Timely Responses\n--------------------------------\n')
-    for key in dict_percentages.keys():
+    for key in students_list:
         txt_file.write('\n' + str(key) + ': ' + str(dict_percentages[key]) + '. '
                        + str(int(responded_array[dict_indices[key]])) + '/'
                        + str(int(questioned_array[dict_indices[key]])) + '.')
